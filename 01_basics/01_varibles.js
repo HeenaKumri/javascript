68f525127daeda64a99695ad6b4545c46935b96b
HeenaKumri <121282085+HeenaKumri@users.noreply.github.com>
2022-12-26 07:07:17 +0000

    heena1

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,21 @@
+const accountId = 20555
+let accountEmail ="heenakumari1482@google.com"
+var AccountPassword ="12345" 
+accountcity ="Ranchi"
+let accountState;
+
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountcity = "Bengalury"
+
+/*
+perfer not to use var
+because of issue in block scope and  functional scope
+
+*/
+
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountcity.accountState])
