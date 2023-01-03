295f3e93e9a791b6c43d0a43854d4493bde2c8ce
HeenaKumri <121282085+HeenaKumri@users.noreply.github.com>
2023-01-03 11:54:42 +0000

    conversion

---
+++ 01_basics/03_Conversionoperation.js
@@ -0,0 +1,20 @@
+let score = 33 // number output
+//let score1 = null // 0 output
+//let score1 = "33" // string output
+//let score1= "33abc" // number NaN output
+//let score1 = undefined //NaN output
+//let score1 = true // 1 output
+let score1 = "Heena" // NaN output
+
+
+
+console.log(typeof score); //number
+console.log(typeof(score));
+
+console.log(typeof score1); //string
+console.log(typeof(score1))
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber );
+console.log(valueInNumber);
+
