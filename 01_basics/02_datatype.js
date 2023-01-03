9eef08312d7008eb00348c142bfff72ee6e8f192
HeenaKumri <121282085+HeenaKumri@users.noreply.github.com>
2023-01-03 07:20:08 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as code as newer version
+
+// alert(3+3) // we are using nodejs, not brower
+
+console .log(3
+    +
+    3) // code readability should be high
+
+console.log(3+3)
+console.log("Heena")
+
+//varible
+let name ="Heena" //string
+let age = 18 //number
+let isLoggedIn = false // boolen
+//end
+
+//dataType
+
+// number => 2 to power 53
+// bigint => bigdata
+// string =>""
+// boolearn => true/false
+// null =>  standalone value /type value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof "Heena")
+console.log(typeof age)
+console.log(typeof null);//object
+console.log(typeof undefined); //undefine
